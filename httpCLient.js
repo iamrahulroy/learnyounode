e45e016d5cfb8440a5bcd1c3af0e394d344bce5b
Rahul Roy <techniciablog@gmail.com>
2019-02-22 00:23:03 +0530

    Add commented out official solution

--- httpCLient.js
+++ httpCLient.js
@@ -4,3 +4,12 @@ http.get(process.argv[2], (response) => {
   response.setEncoding('utf8');
   response.on('data', (data) => console.log(data));
 });
+
+// Offical solution - Compare notes later
+// var http = require('http')
+//
+// http.get(process.argv[2], function (response) {
+//   response.setEncoding('utf8')
+//   response.on('data', console.log)
+//   response.on('error', console.error)
+// }).on('error', console.error)
