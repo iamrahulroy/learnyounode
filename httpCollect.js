6d29bf345ce8bab8ed8e92f94c9e458401735832
Rahul Roy <techniciablog@gmail.com>
2019-02-22 01:33:00 +0530

    Refactor http collect solution

--- httpCollect.js
+++ httpCollect.js
@@ -2,14 +2,12 @@ const http = require('http');
 const bl = require('bl');
 
 http.get(process.argv[2], (response) => {
-  let output = new bl();
+  response.pipe(bl((err, data) => {
+    if (err)
+      return console.error(err);
 
-  response.on('data', (data) => {
-    output.append(data);
-  });
-
-  response.on('end', () => {
-    console.log(output.toString().length)
-    console.log(output.toString())
-  });
+    data = data.toString();
+    console.log(data.length);
+    console.log(data);
+  }));
 });
